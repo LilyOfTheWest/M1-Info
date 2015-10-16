02a1752362a6f9b762a1f906950f1a5689e42d98
LilyOfTheWest <linovergame@hotmail.fr>
2015-10-16 18:42:19 +0200

    C++

--- C++/Exo1/main.cpp
+++ C++/Exo1/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n1, n2;
+
+    cout << "Bonjour connard" << endl;
+    cout << "Entre ton premier nombre ducon :" << endl;
+    cin >> n1;
+    cout << "Entre maintenant ton deuxieme nombre :" << endl;
+    cin >> n2;
+
+    cout << n1 << " + " << n2 << " = " << n1+n2 << endl;
+    cout << "Bye." << endl;
+
     return 0;
 }
