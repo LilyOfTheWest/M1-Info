26393f52c87693c766fd1c7775476cb989e36abf
LilyOfTheWest <linovergame@hotmail.fr>
2015-11-10 15:49:26 +0100

    PROJET

--- TIM/TP/MyTpTIM/TP3/TP_Rehaussement.m
+++ TIM/TP/MyTpTIM/TP3/TP_Rehaussement.m
@@ -26,11 +26,16 @@ dir_ima='..\Image_jpg';
 %nom_ima = 'house.jpg';
 nom_ima = 'fillette.jpg';
 %nom_ima = 'cameraman.jpg';
-%nom_ima = 'venise.jpg';
+%nom_ima = 'graines.jpg';
+%nom_ima = 'ex1_passe-haut.jpg';
 
 %lecture de l'image
 ima=imread([dir_ima '\' nom_ima ]);
+
 ima = double(ima); 
+% Si l'image est en RGB, décommentez la ligne suivante :
+% ima = 0.299*ima(:,:,1)+0.587*ima(:,:,2)+0.114*ima(:,:,3);
+
 
 %dynamique de l'image 
 ima_max = max(ima(:))
@@ -50,8 +55,22 @@ pause
 %Former le noyau du filtre Passe Haut à partir de son opposé Passe Bas
 %--------------- A FAIRE----------------------%
 %Ker_PB = .....; 
-
+taille_kernel = 7;
+choix_filtre = 2;
+if(choix_filtre == 1)
+    Ker_PB = [1 1];
+    for j=1:taille_kernel-2
+        Ker_PB = conv2(Ker_PB, [1 1]);
+    end
+    Ker_PB=Ker_PB'*Ker_PB;
+    Ker_PB = Ker_PB.*(1/(sum(sum(Ker_PB))));
+elseif(choix_filtre == 2)
+    Ker_PB=ones(taille_kernel).*(1/power(taille_kernel,2));
+end
 %Ker_PH = ......;
+Ker_imp = zeros(taille_kernel, taille_kernel);
+Ker_imp((taille_kernel+1)/2, (taille_kernel+1)/2) = 1;
+Ker_PH = Ker_imp - Ker_PB;
 
 %Convolution avec l'image
 ima_PH = conv2(ima, Ker_PH); 
@@ -68,6 +87,7 @@ draw_fig_hist_black_and_white(ima_PH,'image Passe Haut')
 
 %Interpréter l'affichage obtenu. Que faut-il faire pour afficher
 %correctement l'information ?
+% => L'ajouter à l'image d'origine ! (avec le réhaussement)
 
 
 disp('Pause : Rehaussement ')
@@ -78,7 +98,8 @@ pause
 %Former le noyau de convolution du filtre équivalent
 %alpha = ... ; 
 %Ker_RH = ..... ; 
-
+alpha = 1/9;
+Ker_RH = Ker_imp + alpha*Ker_PH;
 %Convolution avec l'image
 ima_RH = conv2(ima, Ker_RH); 
 
@@ -90,13 +111,13 @@ ima_RH_moy = mean(ima_RH(:));
 sprintf('Image réhaussée, max : %7.2f , min : %7.2f , moy : %7.2f \n',ima_RH_max, ima_RH_min, ima_RH_moy)
 
 %AFFICHAGE 
-draw_fig_hist_black_and_white(ima_RH,['image réhaussée, alpha = ' int2str(alpha)])
+draw_fig_hist_black_and_white(ima_RH,['image réhaussée, alpha = ' num2str(alpha)])
 
 %Troncature entre 0 et 255
 ima_RH_tronc = min(max(ima_RH, 0), 255); 
 
 %AFFICHAGE 
-draw_fig_hist_black_and_white(ima_RH_tronc,['image réhaussée, alpha = ' int2str(alpha)])
+draw_fig_hist_black_and_white(ima_RH_tronc,['image réhaussée, alpha = ' num2str(alpha)])
 
 %Interpréter l'affichage obtenu. Mettre en relation l'histogramme à gauche
 %et l'image correspondante à droite. Que constatez vous ? 
@@ -116,6 +137,6 @@ ima_RH2_min = min(ima_RH(:));
 ima_RH2_moy = mean(ima_RH(:));
 
 sprintf('Image réhaussée 2 fois, max : %7.2f , min : %7.2f , moy : %7.2f \n',ima_RH2_max, ima_RH2_min, ima_RH2_moy)
-
+ima_RH2_tronc = min(max(ima_RH2, 0), 255); 
 %AFFICHAGE 
-draw_fig_hist_black_and_white(ima_RH2_tronc,['image réhaussée 2 fois, alpha = ' int2str(alpha)])
+draw_fig_hist_black_and_white(ima_RH2_tronc,['image réhaussée 2 fois, alpha = ' num2str(alpha)])
